26c10affa302fb33392a8bdd45aea1e7cf483337
FarzamN <100859403+MSAlamKhan@users.noreply.github.com>
2023-05-19 16:25:03 +0500

    initial commit

---
+++ src/assets/fonts/PoppinsFont.js
@@ -0,0 +1,15 @@
+export const Font = {
+    Poppins300: 'Poppins-Light',
+    Poppins400: 'Poppins-Regular',
+    Poppins500: 'Poppins-Medium',
+    Poppins600: 'Poppins-SemiBold',
+    Poppins700: 'Poppins-Bold',
+    Poppins800: 'Poppins-ExtraBold',
+    Poppins900: 'Poppins-Black',
+    Libre700: 'LibreBaskerville-Bold',
+    Libre400 : 'LibreBaskerville-Regular',
+    Inter700 : 'Inter-Bold',
+    Inter500 : 'Inter-Medium',
+    GoBold400 : 'Gobold',
+    GoBold300 : 'Gobold-Light'
+  };
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Routing = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Routing;
